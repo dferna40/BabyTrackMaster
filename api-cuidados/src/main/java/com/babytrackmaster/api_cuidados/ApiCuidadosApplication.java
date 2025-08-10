0532c297f440ca8ba95548ffb8cb1348743e5068
David Fernandez Ramirez <dferna40@gmail.com>
2025-08-10 21:47:43 +0200

    Añado configuracion inicial de api-cuidados

---
+++ api-cuidados/src/main/java/com/babytrackmaster/api_cuidados/ApiCuidadosApplication.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_cuidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCuidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCuidadosApplication.class, args);
+	}
+
+}
---
+++ api-cuidados/src/test/java/com/babytrackmaster/api_cuidados/ApiCuidadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_cuidados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCuidadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
