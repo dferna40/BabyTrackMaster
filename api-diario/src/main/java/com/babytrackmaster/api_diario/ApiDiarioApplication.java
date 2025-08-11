2cd81c2a4f3f49685eee2da5337a150a9bd90135
David Fernandez Ramirez <dferna40@gmail.com>
2025-08-11 03:31:32 +0200

    Creo estructura inicial de api-diario

---
+++ api-diario/src/main/java/com/babytrackmaster/api_diario/ApiDiarioApplication.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_diario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDiarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDiarioApplication.class, args);
+	}
+
+}
---
+++ api-diario/src/test/java/com/babytrackmaster/api_diario/ApiDiarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_diario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDiarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
