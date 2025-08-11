da885f93ef0fcb4e16fc7050b240c2c38e706f68
David Fernandez Ramirez <dferna40@gmail.com>
2025-08-11 04:07:48 +0200

    Creo estructura inicial de api-hitos

---
+++ api-hitos/src/main/java/com/babytrackmaster/api_hitos/ApiHitosApplication.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_hitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHitosApplication.class, args);
+	}
+
+}
---
+++ api-hitos/src/test/java/com/babytrackmaster/api_hitos/ApiHitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_hitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
