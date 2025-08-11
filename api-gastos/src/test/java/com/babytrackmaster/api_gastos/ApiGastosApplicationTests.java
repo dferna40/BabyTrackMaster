a1113bdb4de1f801e94e944fca1e106c1e8d1b20
David Fernandez Ramirez <dferna40@gmail.com>
2025-08-11 03:52:37 +0200

    Creo estructura inicial de api-gastos

---
+++ api-gastos/src/test/java/com/babytrackmaster/api_gastos/ApiGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
