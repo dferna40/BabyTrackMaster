29b275d7c872f63cc2d8be7e8177164ebfb0e15d
David Fernandez Ramirez <dferna40@gmail.com>
2025-08-11 04:27:41 +0200

    Creo estructura inicial de api-rutinas

---
+++ api-rutinas/src/main/java/com/babytrackmaster/api_rutinas/ApiRutinasApplication.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_rutinas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRutinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRutinasApplication.class, args);
+	}
+
+}
---
+++ api-rutinas/src/test/java/com/babytrackmaster/api_rutinas/ApiRutinasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_rutinas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRutinasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
