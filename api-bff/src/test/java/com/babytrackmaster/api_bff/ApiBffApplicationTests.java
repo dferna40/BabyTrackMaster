eab00eb184e16475f5dbf9b2a0a4ce9cadc9ca3c
David Fernandez Ramirez <dferna40@gmail.com>
2025-08-12 22:10:10 +0200

    Añado nuevo servicio para unificar llamadas a varias api y devolverlas bajo un unico endpoint

---
+++ api-bff/src/test/java/com/babytrackmaster/api_bff/ApiBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babytrackmaster.api_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
