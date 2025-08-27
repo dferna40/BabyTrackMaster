1dbf9569caed228f5131f96ce57347079bd514bf
David Fernández Ramírez <dferna40@gmail.com>
2025-08-27 16:20:06 +0200

    Añado mas informacion para la pantalla de swagger

--- api-cuidados/src/main/java/com/babytrackmaster/api_cuidados/config/OpenApiConfig.java
+++ api-cuidados/src/main/java/com/babytrackmaster/api_cuidados/config/OpenApiConfig.java
@@ -5,22 +5,29 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
-	@Bean
-	  public OpenAPI openAPI() {
-	    final String schemeName = "bearerAuth";
-	    return new OpenAPI()
-	        .components(new Components().addSecuritySchemes(
-	            schemeName,
-	            new SecurityScheme()
-	                .name(schemeName)
-	                .type(SecurityScheme.Type.HTTP)
-	                .scheme("bearer")
-	                .bearerFormat("JWT")))
-	        .addSecurityItem(new SecurityRequirement().addList(schemeName));
-	  }
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String schemeName = "bearerAuth";
+
+        return new OpenAPI()
+            .info(new Info()
+                .title("BabyTrackMaster Cuidados API") // 👈 Aquí defines el nombre visible
+                .version("v1.0")
+                .description("API para gestionar cuidados de BabyTrackMaster"))
+            .components(new Components().addSecuritySchemes(
+                schemeName,
+                new SecurityScheme()
+                    .name(schemeName)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList(schemeName));
+    }
 }
--- api-usuarios/src/main/java/config/OpenApiConfig.java
+++ api-usuarios/src/main/java/config/OpenApiConfig.java
@@ -1,21 +1,33 @@
 package config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 @Configuration
 public class OpenApiConfig {
-  @Bean
-  public OpenAPI customOpenAPI() {
-    return new OpenAPI()
-      .components(new Components().addSecuritySchemes(
-          "bearerAuth",
-          new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-      ))
-      .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
-  }
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String schemeName = "bearerAuth";
+
+        return new OpenAPI()
+            .info(new Info()
+                .title("BabyTrackMaster Usuarios API") // 👈 Aquí defines el nombre visible
+                .version("v1.0")
+                .description("API para gestionar usuarios de BabyTrackMaster"))
+            .components(new Components().addSecuritySchemes(
+                schemeName,
+                new SecurityScheme()
+                    .name(schemeName)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList(schemeName));
+    }
 }
