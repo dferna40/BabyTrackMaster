c9b6cf04cc93dea56b5b1db22fb40f783b825e57
dferna40 <dferna40@gmail.com>
2025-08-28 17:35:12 +0200

    feat: actualizar DTO de gastos y controlador

--- api-bff/src/main/java/com/babytrackmaster/api_bff/dto/GastoDTO.java
+++ api-bff/src/main/java/com/babytrackmaster/api_bff/dto/GastoDTO.java
@@ -1,15 +1,22 @@
 package com.babytrackmaster.api_bff.dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GastoDTO {
   private Long id;
-  private String categoria;
+  private Long categoriaId;
+  private BigDecimal cantidad;
+  private LocalDate fecha;
   private String descripcion;
-  private Double importe;
-  private String fecha; // yyyy-MM-dd
+  private Long bebeId;
 }
