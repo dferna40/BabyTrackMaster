e763b2d986e590c194f82e1abe4aa06d9edae566
David Fernández Ramírez <dferna40@gmail.com>
2025-08-29 02:58:25 +0200

    Creo estructura inicial del frontend-baby

--- frontend-baby/src/App.js
+++ frontend-baby/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignIn from "./pages/SignIn";
+import signInSide from "./sign-in-side";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<signInSide />} />
       </Routes>
     </Router>
   );
--- frontend-baby/src/pages/Login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Login() {
-  return <h2>Pantalla de Login</h2>;
-}
-
-export default Login;
