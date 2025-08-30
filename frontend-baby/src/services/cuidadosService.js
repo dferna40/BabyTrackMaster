04f029b4a2556ec3c1af79211aada7fd2ce45647
dferna40 <dferna40@gmail.com>
2025-08-30 20:04:30 +0200

    feat(frontend): add cuidados service

---
+++ frontend-baby/src/services/cuidadosService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import API_BASE_URL from '../config';
+
+const API_CUIDADOS_URL = `${API_BASE_URL}/api/v1/cuidados`;
+
+export const listarPorBebe = (bebeId) => {
+  return axios.get(`${API_CUIDADOS_URL}/bebe/${bebeId}`);
+};
+
+export const crearCuidado = (data) => {
+  return axios.post(`${API_CUIDADOS_URL}`, data);
+};
+
+export const actualizarCuidado = (id, data) => {
+  return axios.put(`${API_CUIDADOS_URL}/${id}`, data);
+};
+
+export const eliminarCuidado = (id) => {
+  return axios.delete(`${API_CUIDADOS_URL}/${id}`);
+};
+
