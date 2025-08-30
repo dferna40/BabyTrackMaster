26d20471336f283cee6844b6591f6a19231728bc
dferna40 <dferna40@gmail.com>
2025-08-30 17:48:50 +0200

    feat: add TopBar component

--- frontend-baby/src/dashboard/components/Header.js
+++ frontend-baby/src/dashboard/components/Header.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
-import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
 import CustomDatePicker from './CustomDatePicker';
-import NavbarBreadcrumbs from './NavbarBreadcrumbs';
-import MenuButton from './MenuButton';
+import TopBar from './TopBar';
 import ColorModeIconDropdown from '../../shared-theme/ColorModeIconDropdown';
-
 import Search from './Search';
 
 export default function Header() {
@@ -22,13 +19,10 @@ export default function Header() {
       }}
       spacing={2}
     >
-      <NavbarBreadcrumbs />
+      <TopBar />
       <Stack direction="row" sx={{ gap: 1 }}>
         <Search />
         <CustomDatePicker />
-        <MenuButton showBadge aria-label="Open notifications">
-          <NotificationsRoundedIcon />
-        </MenuButton>
         <ColorModeIconDropdown />
       </Stack>
     </Stack>
