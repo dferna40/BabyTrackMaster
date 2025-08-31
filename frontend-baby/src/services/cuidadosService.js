d9c8c3f928cc06ac248a5e8b75ab3c200eff5dea
dferna40 <dferna40@gmail.com>
2025-08-31 16:29:38 +0200

    feat(frontend): support multiple API endpoints

--- frontend-baby/src/services/cuidadosService.js
+++ frontend-baby/src/services/cuidadosService.js
@@ -1,30 +1,30 @@
 import axios from 'axios';
-import API_BASE_URL from '../config';
+import { API_CUIDADOS_URL } from '../config';
 
-const API_CUIDADOS_URL = `${API_BASE_URL}/api/v1/cuidados`;
+const API_CUIDADOS_ENDPOINT = `${API_CUIDADOS_URL}/api/v1/cuidados`;
 
 export const listarPorBebe = (bebeId, page, size) => {
   const params = {};
   if (page !== undefined) params.page = page;
   if (size !== undefined) params.size = size;
-  return axios.get(`${API_CUIDADOS_URL}/bebe/${bebeId}`, { params });
+  return axios.get(`${API_CUIDADOS_ENDPOINT}/bebe/${bebeId}`, { params });
 };
 
 export const listarRecientes = (bebeId, limit = 5) => {
-  return axios.get(`${API_CUIDADOS_URL}/bebe/${bebeId}`, {
+  return axios.get(`${API_CUIDADOS_ENDPOINT}/bebe/${bebeId}`, {
     params: { limit },
   });
 };
 
 export const crearCuidado = (data) => {
-  return axios.post(`${API_CUIDADOS_URL}`, data);
+  return axios.post(`${API_CUIDADOS_ENDPOINT}`, data);
 };
 
 export const actualizarCuidado = (id, data) => {
-  return axios.put(`${API_CUIDADOS_URL}/${id}`, data);
+  return axios.put(`${API_CUIDADOS_ENDPOINT}/${id}`, data);
 };
 
 export const eliminarCuidado = (id) => {
-  return axios.delete(`${API_CUIDADOS_URL}/${id}`);
+  return axios.delete(`${API_CUIDADOS_ENDPOINT}/${id}`);
 };
 
