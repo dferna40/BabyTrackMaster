73904c118fec825d672593d7c7a389e69f924f8c
dferna40 <dferna40@gmail.com>
2025-08-31 12:14:21 +0200

    feat: add gastos service

---
+++ frontend-baby/src/services/gastosService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import API_BASE_URL from '../config';
+
+const API_GASTOS_URL = `${API_BASE_URL}/api/v1/gastos`;
+
+export const listarPorBebe = (bebeId, page, size) => {
+  const params = {};
+  if (page !== undefined) params.page = page;
+  if (size !== undefined) params.size = size;
+  return axios.get(`${API_GASTOS_URL}/bebe/${bebeId}`, { params });
+};
+
+export const listarRecientes = (bebeId, limit = 5) => {
+  return axios.get(`${API_GASTOS_URL}/bebe/${bebeId}`, {
+    params: { limit },
+  });
+};
+
+export const crearGasto = (data) => {
+  return axios.post(`${API_GASTOS_URL}`, data);
+};
+
+export const actualizarGasto = (id, data) => {
+  return axios.put(`${API_GASTOS_URL}/${id}`, data);
+};
+
+export const eliminarGasto = (id) => {
+  return axios.delete(`${API_GASTOS_URL}/${id}`);
+};
+
