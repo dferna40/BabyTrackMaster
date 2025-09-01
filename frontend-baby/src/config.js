dc2641e646606f6a14eb802dc6f97852e83b4c05
dferna40 <dferna40@gmail.com>
2025-09-01 02:47:01 +0200

    feat: add baby service URL configuration

--- frontend-baby/src/config.js
+++ frontend-baby/src/config.js
@@ -12,6 +12,8 @@ const API_DIARIO_URL =
   process.env.REACT_APP_API_DIARIO_URL || 'http://localhost:8085';
 const API_RUTINAS_URL =
   process.env.REACT_APP_API_RUTINAS_URL || 'http://localhost:8086';
+const API_BEBE_URL =
+  process.env.REACT_APP_API_BEBE_URL || 'http://localhost:8089';
 
 export {
   API_USUARIOS_URL,
@@ -21,5 +23,6 @@ export {
   API_CITAS_URL,
   API_DIARIO_URL,
   API_RUTINAS_URL,
+  API_BEBE_URL,
 };
 
