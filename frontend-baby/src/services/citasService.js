afbc43e87ddd6ce39a2250cdc3fdf681897db3fd
dferna40 <dferna40@gmail.com>
2025-09-03 16:08:51 +0200

    feat: update citas service endpoints

--- frontend-baby/src/services/citasService.js
+++ frontend-baby/src/services/citasService.js
@@ -2,28 +2,29 @@ import axios from 'axios';
 import { API_CITAS_URL } from '../config';
 
 const API_CITAS_ENDPOINT = `${API_CITAS_URL}/api/v1/citas`;
+const API_TIPOS_CITA_ENDPOINT = `${API_CITAS_URL}/api/v1/tipos-cita`;
 
-export const listarPorBebe = (usuarioId, bebeId) => {
-  return axios.get(`${API_CITAS_ENDPOINT}/usuario/${usuarioId}/bebe/${bebeId}`);
+export const listar = () => {
+  return axios.get(`${API_CITAS_ENDPOINT}`);
 };
 
-export const crearCita = (usuarioId, data) => {
-  return axios.post(`${API_CITAS_ENDPOINT}/usuario/${usuarioId}`, data);
+export const crearCita = (data) => {
+  return axios.post(`${API_CITAS_ENDPOINT}`, data);
 };
 
-export const actualizarCita = (usuarioId, id, data) => {
-  return axios.put(`${API_CITAS_ENDPOINT}/usuario/${usuarioId}/${id}`, data);
+export const actualizarCita = (id, data) => {
+  return axios.put(`${API_CITAS_ENDPOINT}/${id}`, data);
 };
 
-export const eliminarCita = (usuarioId, id) => {
-  return axios.delete(`${API_CITAS_ENDPOINT}/usuario/${usuarioId}/${id}`);
+export const eliminarCita = (id) => {
+  return axios.delete(`${API_CITAS_ENDPOINT}/${id}`);
 };
 
 export const listarTipos = () => {
-  return axios.get(`${API_CITAS_ENDPOINT}/tipos`);
+  return axios.get(`${API_TIPOS_CITA_ENDPOINT}`);
 };
 
-export const enviarRecordatorio = (usuarioId, id) => {
-  return axios.post(`${API_CITAS_ENDPOINT}/usuario/${usuarioId}/${id}/recordatorio`);
+export const enviarRecordatorio = (id) => {
+  return axios.post(`${API_CITAS_ENDPOINT}/${id}/recordatorio`);
 };
 
