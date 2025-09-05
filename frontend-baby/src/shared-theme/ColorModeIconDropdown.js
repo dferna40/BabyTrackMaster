ea37aba7a550df5f09ba4cdffe3eb9312edbe37f
dferna40 <dferna40@gmail.com>
2025-09-05 15:26:49 +0200

    feat: translate color mode labels to Spanish

--- frontend-baby/src/shared-theme/ColorModeIconDropdown.js
+++ frontend-baby/src/shared-theme/ColorModeIconDropdown.js
@@ -75,13 +75,13 @@ export default function ColorModeIconDropdown(props) {
         anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
         <MenuItem selected={mode === 'system'} onClick={handleMode('system')}>
-          System
+          Sistema
         </MenuItem>
         <MenuItem selected={mode === 'light'} onClick={handleMode('light')}>
-          Light
+          Claro
         </MenuItem>
         <MenuItem selected={mode === 'dark'} onClick={handleMode('dark')}>
-          Dark
+          Oscuro
         </MenuItem>
       </Menu>
     </React.Fragment>
