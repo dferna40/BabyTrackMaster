7b6513390517480f963b6ea306a9c96e35952267
dferna40 <dferna40@gmail.com>
2025-09-06 02:48:51 +0200

    Include bebeId in alimentacion record operations

--- frontend-baby/src/services/alimentacionService.js
+++ frontend-baby/src/services/alimentacionService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_ALIMENTACION_URL } from '../config';
 
-const API_ALIMENTACION_ENDPOINT = `${API_ALIMENTACION_URL}/api/v1/alimentaciones`;
+const API_ALIMENTACION_ENDPOINT = `${API_ALIMENTACION_URL}/api/v1/alimentacion`;
 
 export const listarPorBebe = (usuarioId, bebeId) => {
   return axios.get(
@@ -24,19 +24,19 @@ export const obtenerEstadisticas = (usuarioId, bebeId) => {
   );
 };
 
-export const crearRegistro = (usuarioId, data) => {
-  return axios.post(`${API_ALIMENTACION_ENDPOINT}/usuario/${usuarioId}`, data);
-};
-
-export const actualizarRegistro = (usuarioId, id, data) => {
-  return axios.put(
-    `${API_ALIMENTACION_ENDPOINT}/usuario/${usuarioId}/${id}`,
+export const crearRegistro = (usuarioId, bebeId, data) =>
+  axios.post(
+    `${API_ALIMENTACION_ENDPOINT}/usuario/${usuarioId}/bebe/${bebeId}`,
     data
   );
-};
 
-export const eliminarRegistro = (usuarioId, id) => {
-  return axios.delete(
-    `${API_ALIMENTACION_ENDPOINT}/usuario/${usuarioId}/${id}`
+export const actualizarRegistro = (usuarioId, bebeId, id, data) =>
+  axios.put(
+    `${API_ALIMENTACION_ENDPOINT}/usuario/${usuarioId}/bebe/${bebeId}/${id}`,
+    data
   );
-};
+
+export const eliminarRegistro = (usuarioId, bebeId, id) =>
+  axios.delete(
+    `${API_ALIMENTACION_ENDPOINT}/usuario/${usuarioId}/bebe/${bebeId}/${id}`
+  );
