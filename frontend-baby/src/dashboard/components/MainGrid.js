c1ac1ace4e5e79c68133e81e45cf966fd566d775
dferna40 <dferna40@gmail.com>
2025-09-07 09:39:23 +0200

    feat(dashboard): reorganize main grid layout

--- frontend-baby/src/dashboard/components/MainGrid.js
+++ frontend-baby/src/dashboard/components/MainGrid.js
@@ -3,13 +3,10 @@ import { Navigate } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Copyright from '../internals/components/Copyright';
-import DailyRoutinesCard from './DailyRoutinesCard';
 import UpcomingAppointmentsCard from './UpcomingAppointmentsCard';
 import RecentCareCard from './RecentCareCard';
-import QuickStatsCard from './QuickStatsCard';
 import HighlightedCard from './HighlightedCard';
 import QuickActionsCard from './QuickActionsCard';
-import StatusSummaryCard from './StatusSummaryCard';
 import { BabyContext } from '../../context/BabyContext';
 
 export default function MainGrid() {
@@ -20,28 +17,26 @@ export default function MainGrid() {
   }
 
   return (
-    <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: { sm: '100%', md: '1700px' },
+        mx: 'auto',
+        p: 2,
+      }}
+    >
       <Grid container spacing={2}>
-        <Grid size={{ xs: 12 }}>
+        <Grid size={{ xs: 12, md: 6 }}>
           <HighlightedCard />
         </Grid>
-        <Grid size={{ xs: 12 }}>
+        <Grid size={{ xs: 12, md: 6 }}>
           <QuickActionsCard />
-        </Grid>
-        <Grid size={{ xs: 12 }}>
-          <StatusSummaryCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <DailyRoutinesCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <UpcomingAppointmentsCard />
         </Grid>
         <Grid size={{ xs: 12, md: 6 }}>
           <RecentCareCard />
         </Grid>
         <Grid size={{ xs: 12, md: 6 }}>
-          <QuickStatsCard />
+          <UpcomingAppointmentsCard />
         </Grid>
       </Grid>
       <Copyright sx={{ my: 4 }} />
